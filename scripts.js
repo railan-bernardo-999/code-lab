bcf05f9e88e87681617ec3a713d42fd0f1620f92
Railan Bernardo <railan.bernardo@syonet.com>
2025-07-03 21:45:32 -0300

    remove icon

--- scripts.js
+++ scripts.js
@@ -30,7 +30,6 @@ function notification() {
     if (Notification.permission === "granted") {
         new Notification("⏰ Timer finalizado!", {
             body: "O seu tempo acabou.",
-            icon: "https://cdn-icons-png.flaticon.com/512/2088/2088617.png"
         });
     }else{
         alert("Notificações não estão permitidas.");
